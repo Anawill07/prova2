2f2988674d0be57f2dd56449228ebc6ef2e07afd
ANA CAROLINA WILL <ana_carolina-will@estudante.sesisenai.org.br>
2025-05-22 09:41:16 -0300

    rota para consultar o log

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ const mensagem = `${idUnico}, ${dataHora}, ${nomeAluno}/n`;
 fs.appendFile('logs.txt', mensagem, 'utf8', (err) => {
 });
 }
+
+fs.appendFile('logs.txt', mensagem, 'utf8', (err) => {
+  if (err){
+      console.error('Erro para escrever no arquivo:', err);
+  }else{
+      console.log('Adicionado com sucesso');
+  }
+});
